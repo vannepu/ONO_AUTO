6bf566d949bed426e897669cd30c2376d787b294
vannepu <vannepu@users.noreply.github.com>
2026-08-24 12:09:31 -0700

    add onologin test

---
+++ test_onologin.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def test_onologin(playwright: Playwright):
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://dev-mos.onoark.com/languageSelection")
+    page.get_by_role("heading",name="En").click()
+    page.get_by_role("textbox").fill("1000000000 ")
+    page.get_by_role("button", name="LOGIN").click()
+    page.get_by_role("textbox").fill("123456")
+    page.get_by_role("button", name="CONTINUE").click()
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+
+# Press the green button in the gutter to run the script.
+with sync_playwright() as playwright:
+    test_onologin(playwright)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
